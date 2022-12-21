c016b60b290e3cb799994fdaec317a5670d0ef54
JPLA-DEVELOPER <jpla.developer@gmail.com>
2022-12-20 22:03:44 -0300

    02

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Dependencias do projeto
+import requests
+from bs4 import BeautifulSoup
+import tabulate
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+headers = {'user-Agent': 'Mozila/5.0'} #Permite fazer o acesso ao servidor como se fosse um navegador web.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+resposta = requests.get('https://www.fundamentus.com.br/fii_resultado.php', headers=headers) #Faz a requisição na página especificada.
+
+soup = BeautifulSoup(resposta.text, 'html.parser')
+
+
+linhas = soup.find(id='tabelaResultado').find('tbody').find_all('tr') #Pega todas as linhas da tabela exceto o cabeçalho
+
+for linha in linhas:
+    dados_fundo = linha.find_all('td')#busca todas as células que estão dentro de cada linha.
+    print(
+        f'[ {dados_fundo[2].text}]\n'
+        f'\tCotação:[ {dados_fundo[1].text}]\n'
+        f'\tSetor:[ {dados_fundo[4].text}]\n'
+        f'\tDY:[ {dados_fundo[5].text}]\n'
+        f'\tP/VP:[ {dados_fundo[5].text}]\n'
+          )
+
+
+
+
